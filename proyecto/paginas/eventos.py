29104214b9077a521da9aedf90496c557a807079
SaraPazo <sarapazop@gmail.com>
2023-12-03 17:16:06 +0100

    guardo

--- proyecto/paginas/eventos.py
+++ proyecto/paginas/eventos.py
@@ -23,19 +23,25 @@ st.title('¿Qué te gustaría hacer?')
 
 event = pd.read_csv('../csv/eventos_fin.csv')
 
-barrio, mes, dia = st.columns(3)
+# Dynamic price range
+price_min, price_max = event.precio.min(), event.precio.max()
+
+barrio, precio, mes, dia = st.columns(4)
 
 with barrio:
     sel1 = st.multiselect('Elige tu barrio', event.barrio.unique())
 
+with precio:
+    sel2 = st.slider('Rango de precio', price_min, price_max, (price_min, price_max))
+                            
 with mes:
-    sel2 = st.selectbox('En qué mes estás pensando?', event.mes.unique())
+    sel3 = st.selectbox('En qué mes estás pensando?', event.mes.unique())
 
 with dia:
-    sel3 = st.multiselect('Y... qué día te viene bien?', event.dia.unique().tolist())
+    sel4 = st.multiselect('Y... qué día te viene bien?', event.dia.unique().tolist())
 
 
-evento = event[(event.barrio.isin(sel1)) & (event.mes == sel2) & (event.dia.isin(sel3))]
+evento = event[(event.barrio.isin(sel1)) & (event.precio.between(sel2[0], sel2[1])) & (event.mes == sel3) & (event.dia.isin(sel4))]
 
 if not evento.empty:
     # Mostrar el DataFrame resultante
@@ -44,8 +50,9 @@ if not evento.empty:
     # Crear una nueva columna con enlaces clicables
     def_event['enlace_clicable'] = def_event['enlace'].apply(lambda x: f'<a href="{x}" target="_blank">{x}</a>')
 
-    # Mostrar el DataFrame con la columna de enlaces clicables
+    # Mostrar el DataFrame con la columna de enlaces clicables    
     st.write(def_event[['nombre', 'local', 'barrio', 'ubicacion', 'precio', 'mes', 'dia', 'enlace_clicable']], unsafe_allow_html=True)
+
 else:
     # Mostrar un mensaje indicando que no hay resultados
     st.write("No hay eventos que cumplan con los criterios seleccionados.")
---
+++ proyecto/paginas/mapas.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import webbrowser
+import base64
+import io
+import datetime
+from streamlit_folium import st_folium
+from streamlit_folium import folium_static
+import folium
+
+
+st.set_page_config(page_title='Tu experiencia - Mapa', page_icon="🌍", layout="wide")
+
+# ----- LOAD COSILLAS ---- 
+
+poster_contact_form = Image.open('../imagenes/poster.jpeg')
+
+# --- head ---
+
+
+st.image(poster_contact_form, width = 200)
+
+st.title('A dónde te gustaría ir?')
+
+event_map = pd.read_csv('../mapa/csv_map/eventos_map.csv')
+madrid_map = pd.read_csv('../mapa/csv_map/madrid_map.csv')
+restaurantes_map = pd.read_csv('../mapa/csv_map/restaurantes_map.csv')
+
+map, selection = st.columns(2)
+
+with map:
+    st.subheader("¿Tu ubicación en el mapa?")
+    st.write("##")
+    mapa = folium.Map(location=[40.416709, -3.690286], zoom_start=17, color='#0044ff')
+
+    for i in range(0, len(restaurantes_map)):
+        folium.Marker([float(restaurantes_map.lat[i]), float(restaurantes_map.long[i])], 
+                popup=restaurantes_map.nombre[i],
+                icon = folium.Icon(color='darkgreen')).add_to(mapa)
+
+    for i in range(0, len(event_map)):
+        folium.Marker([float(event_map.lat[i]), float(event_map.long[i])], 
+                popup = event_map.nombre[i],
+                icon = folium.Icon(color='red')).add_to(mapa)
+
+    st_folium(mapa, height = 550, width = 1200)
+
+
+with selection:
+    st.header('¿Cuál es tu barrio favorito?')
+
+    barrio, precio = st.columns(2)
+
+    with barrio:
+        sel1 = st.multiselect('Elige tu barrio', event_map.barrio.unique())
+
+    with precio:
+        sel2 = st.selectbox('Elige rango de precio', event_map.precio.unique())
+
+    
+    evento = event_map[(event_map.barrio.isin(sel1)) & (event_map.precio == sel2)]
+
+    if not evento.empty:
+        # Mostrar el DataFrame resultante
+        def_event = evento[['nombre', 'barrio', 'precio']]
+        st.dataframe(def_event, width = 700)
+
+    else:
+        # Mostrar un mensaje indicando que no hay resultados
+        st.write("No hay eventos que cumplan con los criterios seleccionados.")
+
+
+
+ #rest = restaurantes_map[(restaurantes_map.barrio.isin(sel1))]
